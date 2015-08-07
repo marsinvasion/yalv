59abac1968d5f2cba55068e4587f9e7d9800686d
Anoop Kulkarni <anoop.kulkarni@gmail.com>
2015-08-07 11:56:42 -0700

    os info

---
+++ os.js
@@ -0,0 +1,40 @@
+var os = require('os');
+
+var info = {};
+info.hostname = os.hostname();
+info.type=os.type();
+info.platform = os.platform();
+info.architecture=os.arch();
+info.release=os.release();
+info.uptime = os.uptime();
+if(info.uptime >= 86400){
+  info.uptime = info.uptime/86400+ " days";
+}else if(info.uptime>=3600){
+  info.uptime = info.uptime/3600 + " hours";
+}else if(info.uptime>=60){
+  info.uptime = info.uptime/60 + " minutes";
+}else{
+  info.uptime = info.uptime/1000 + " seconds";
+}
+
+var humanMem = function(mem){
+  if(mem>1073741824){
+    return mem/1073741824 + " GB";
+  }else if(mem>1048576){
+    return mem/1048576 + " MB";
+  }else return mem;
+}
+var totalMem = os.totalmem();
+info.totalMem = humanMem(os.totalmem());
+
+var load = function(){
+  console.log("******");
+  info.date=Date.now();
+  info.loadAverage=os.loadavg();
+  info.freeMemory=humanMem(os.freemem());
+  info.memoryUsed=humanMem(totalMem-os.freemem());
+  console.log(info);
+}
+
+
+setInterval(load,1000);

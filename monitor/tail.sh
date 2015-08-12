f1759efdc1585137427ec18f88dd02ae3ca3a49b
Anoop Kulkarni <anoop.kulkarni@gmail.com>
2015-08-12 12:32:13 -0700

    pass in udp port as argument

--- monitor/tail.sh
+++ monitor/tail.sh
@@ -8,10 +8,14 @@ str=$2
 if [ -z "$str" ]; then
   str=pid
 fi
+udpPort=$3
+if [ -z "$udpPort"]; then
+  udpPort=5201
+fi
 echo "tailing $fileName searching for $str"
 tail -f --retry $fileName 2>/dev/null |
   while read line; do
     if [[ $line == *$str* ]]; then
-      echo -n "$line" > /dev/udp/localhost/5201
+      echo -n "$line" > /dev/udp/localhost/$udpPort
     fi
   done

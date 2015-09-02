40dfa3eedce611c79d5ae30057aeffdf788936be
Anoop Kulkarni <anoop.kulkarni@gmail.com>
2015-09-02 10:51:33 -0700

    add a few more logs

--- logGenerator/generate.js
+++ logGenerator/generate.js
@@ -19,15 +19,19 @@ var simulate = function(){
   var reqId = crypto.randomBytes(10).toString('hex');
   logStr('entering',reqId,'web server','web call');
   logStr('entering',reqId,'user','getUser');
+  logStr('entering',reqId,'service','setup calculation');
   logStr('entering',reqId,'service','calculate');
   logStr('entering',reqId,'service','calculate');//simulate loop
+  logStr('entering',reqId,'db','getUser');
   logStr('entering',reqId,'db','getData');
 
   setTimeout(logStr,Math.random() * 10000,'exiting',reqId,'web server','web call');
   setTimeout(logStr,Math.random() * 1000,'exiting',reqId,'user','getUser');
+  setTimeout(logStr,Math.random() * 2000,'exiting',reqId,'service','setup calculation');
   setTimeout(logStr,Math.random() * 2000,'exiting',reqId,'service','calculate');
   setTimeout(logStr,Math.random() * 2000,'exiting',reqId,'service','calculate');
   setTimeout(logStr,Math.random() * 5000,'exiting',reqId,'db','getData');
+  setTimeout(logStr,Math.random() * 1000,'exiting',reqId,'db','getUser');
 }
 
 module.exports = {

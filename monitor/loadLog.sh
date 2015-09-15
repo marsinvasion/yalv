ac16996d0d94c32618c08a97b235dea099d099a2
Anoop Kulkarni <anoop.kulkarni@gmail.com>
2015-09-15 09:43:33 -0700

    load log files

---
+++ monitor/loadLog.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+fileName=$1
+if [ -z "$fileName" ]; then
+  fileName=app.log
+fi
+str=$2
+if [ -z "$str" ]; then
+  str=pid
+fi
+udpPort=$3
+if [ -z "$udpPort" ]; then
+  udpPort=5201
+fi
+echo "loading $fileName searching for $str"
+while read -r line || [[ -n "$line" ]]; do
+  if [[ $line == *$str* ]]; then
+      echo -n "$line" > /dev/udp/localhost/$udpPort
+  fi
+done < "$fileName"
